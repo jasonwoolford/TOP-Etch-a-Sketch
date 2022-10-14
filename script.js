23d0f68e54d0e001efe9277f219f6460bb126033
jasonwoolford <jason.woolford115@gmail.com>
2022-10-14 07:56:11 -0400

    There is now a grid on screen

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const body = document.body;
-
 const mainContainer = document.createElement('div')
 document.body.appendChild(mainContainer);
 
-const div2 = document.createElement('div');
-div2.classList.add('div2');
-mainContainer.appendChild(div2);
+const gridbox = document.createElement('div');
+gridbox.querySelector('#gridbox')
+gridbox.classList.add('gridbox');
+mainContainer.appendChild(gridbox);
 
-const p1 = document.createElement('p');
-p1.classList.add('p1');
-p1.textContent = "Hello world!"
-div2.appendChild(p1);
+const grid = document.createElement('div')
+gridbox.appendChild(grid);
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+
+defaultGrid();
+//Creates a default grid sized 16x16 
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+//Grid creation logic
+function makeRows(rowNum) {
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        grid.appendChild(row).className = "gridRow";
+    };
+};
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+    };
+};
+
+const resetButton = document.createElement('button');
+resetButton.classList.add('resetButton');
+resetButton.textContent = "Reset Grid";
+mainContainer.appendChild(resetButton);
+
+resetButton.addEventListener('click', function () {
+    defaultGrid();
+});

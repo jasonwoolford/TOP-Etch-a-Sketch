2204641b6b10c604a8f7ed5a727b5394c0979778
jasonwoolford <jason.woolford115@gmail.com>
2022-10-14 08:46:20 -0400

    Hover rules added for the divs in grid

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const mainContainer = document.createElement('div')
 document.body.appendChild(mainContainer);
 
-const gridbox = document.createElement('div');
-gridbox.querySelector('#gridbox')
-gridbox.classList.add('gridbox');
-mainContainer.appendChild(gridbox);
-
 const grid = document.createElement('div')
 gridbox.appendChild(grid);
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
-
-
-defaultGrid();
-//Creates a default grid sized 16x16 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
 
 //Grid creation logic
 function makeRows(rowNum) {
@@ -36,11 +23,19 @@ function makeColumns(cellNum) {
     };
 };
 
+//Creates a default grid sized 16x16 
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
 const resetButton = document.createElement('button');
 resetButton.classList.add('resetButton');
 resetButton.textContent = "Reset Grid";
 mainContainer.appendChild(resetButton);
 
 resetButton.addEventListener('click', function () {
+    makeRows(0);
+    makeColumns(0);
     defaultGrid();
 });
